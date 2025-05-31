a9316a788d3b99c3fcb65d2f1df7099dfa4489ba
mingmoe <me@kawayi.moe>
2025-05-31 21:28:13 +0800

    Initial commit

---
+++ Autofac.Extension/Autofac.Extension/ConfigurationHelper.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Autofac.Extension.ConfigurationHelper;
+
+namespace Autofac.Extension;
+public static class ConfigurationHelper
+{
+    internal class Configuration<T>
+    {
+        public required Action<T> ConfigureAction { get; init; }
+    }
+
+    public static void AddOptions(this ContainerBuilder builder)
+    {
+        builder.RegisterGeneric(typeof(OptionsWrapper<>)).As(typeof(IOptions<>));
+    }
+
+    public static void Configure<T>(this ContainerBuilder builder, Action<T> configure)
+        where T : class, new()
+    {
+        builder.RegisterInstance<Configuration<T>>(new Configuration<T>()
+        {
+            ConfigureAction = configure
+        }).AsSelf().SingleInstance();
+
+        builder.Register<T>((context) =>
+        {
+            T options = new();
+            context.Resolve<IEnumerable<Configuration<T>>>()
+                .ToList()
+                .ForEach(c => c.ConfigureAction(options));
+            return options;
+        }).AsSelf().SingleInstance();
+    }
+}
---
+++ Autofac.Extension/Sample/Program.cs
@@ -0,0 +1,62 @@
+﻿using Autofac;
+using Autofac.Diagnostics.DotGraph;
+using Autofac.Extension;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace Sample;
+
+internal class Program
+{
+    class A
+    {
+        public required string Name { get; init; }
+
+        public required object Value { get; init; }
+
+        public required IOptions<C> Options { get; init; }
+
+        public required ILogger<A> Logger { get; init; }
+
+        public void Print()
+        {
+            Logger.LogInformation($"A: {Name}, Value: {Value}, Configure Name: {Options.Value.Name}");
+        }
+    }
+
+    class C
+    {
+        public string Name { get; set; } = "Problem with ConfigurationHelper!";
+    }
+
+    static void Main(string[] args)
+    {
+        var containerBuilder = new ContainerBuilder();
+
+        containerBuilder.AddLogging(static (f) =>
+        {
+            f.AddConsole();
+        });
+        containerBuilder.AddOptions();
+        containerBuilder.Configure<C>((c) =>
+        {
+            c.Name = "ConfigurationHelper's first configuration!";
+        });
+        containerBuilder.Configure<C>((c) =>
+        {
+            c.Name = "ConfigurationHelper's second configuration!";
+        });
+
+        containerBuilder.Register(ctx => "Hello");
+        containerBuilder.RegisterInstance<object>(1);
+        containerBuilder.RegisterType<A>().AsSelf();
+        var container = containerBuilder.Build();
+
+        container.OutputDotGraph();
+
+        using var scope = container.BeginLifetimeScope();
+        scope.Resolve<A>().Print();
+    }
+}
